0431fb9f1571b0e5823431f88f22cf180fdd2c78
hello382 <68135550+hello382@users.noreply.github.com>
2022-08-06 11:32:43 +0800

    Update site-config.ts

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,11 +1,11 @@
 import * as types from './types'
 
 export interface SiteConfig {
-  rootNotionPageId: string
+  rootNotionPageId: 4efa5622aac84cfe895cab8ee2b51456
   rootNotionSpaceId?: string
 
-  name: string
-  domain: string
+  name: 荣的博客
+  domain: https://www.notion.so/123-4efa5622aac84cfe895cab8ee2b51456
   author: string
   description?: string
   language?: string
